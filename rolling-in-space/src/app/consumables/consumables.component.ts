9fd274ecbcf4f3c4c7b2a423e4caa4b0d9ee16dd
hleonov <blackbirdy@gmail.com>
2021-09-23 16:52:58 +0200

    refactor consumables form handling into the consumables component

--- rolling-in-space/src/app/consumables/consumables.component.ts
+++ rolling-in-space/src/app/consumables/consumables.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
-import { CharacterConsumables } from '../models/entity/character-consumables';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
+import { StatsDto } from '../models/dto/StatsDto';
+import { CharacterConsumables, Consumable } from '../models/entity/character-consumables';
 import { WebsocketService } from "../services/websocket.service";
 
 @Component({
@@ -12,44 +14,97 @@ import { WebsocketService } from "../services/websocket.service";
 })
 export class ConsumablesComponent implements OnInit {
   @Input() currentChar : CharacterConsumables;
-  @Input() parentStatsForm : FormGroup;
   public pointsToAllocate = 0;
-  constructor() {
+  public statForm : FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    public webSocketService: WebsocketService) {
+
   }
   
   ngOnInit(): void{
-    console.log( "got parent form: " + this.parentStatsForm )
+    this.webSocketService.connect();
+    this.webSocketService.statChangedEvents.subscribe(dto => {
+      this.handleStatsChange(dto);
+    })
+    this.addStatsForm(this.currentChar)
   }
 
-  ngAfterViewInit (): void {
+  ngOnDestroy(): void {
+    this.webSocketService.disconnect();
   }
 
-  // validate(event : any) {
-  //   var theEvent = event || window.event;
-  //   // Handle paste
-  //   if (theEvent.type === 'paste') {
-  //       key = theEvent.clipboardData.getData('text/plain');
-  //   } else {
-  //   // Handle key press
-  //       var key = theEvent.keyCode || theEvent.which;
-  //       key = String.fromCharCode(key);
-  //   }
-  //   var regex = /[0-9]|\./;
-  //   if( !regex.test(key) ) {
-  //     theEvent.returnValue = false;
-  //     if(theEvent.preventDefault) theEvent.preventDefault();
-  //   }
-  // }
+  addStatsForm(stat : CharacterConsumables) {
+    let numberRegEx =     /^[0-9]\d*$/g ;// /^\d+$/;
+    let validators = [Validators.required, Validators.pattern(numberRegEx)];
+    this.statForm = this.formBuilder.group({
+      mightMax: [stat.might.maxValue, validators],
+      mightCur: [stat.might.currentValue, validators],
+      speedMax: [stat.speed.maxValue, validators],
+      speedCur: [stat.speed.currentValue, validators],
+      intellectMax: [stat.intellect.maxValue, validators],
+      intellectCur: [stat.intellect.currentValue, validators],
+      stressMax: [stat.stress.maxValue, validators],
+      stressCur: [stat.stress.currentValue, validators],
+      mentalMax: [stat.mental.maxValue,  validators],
+      mentalCur: [stat.mental.currentValue,  validators],
+      timesRested: [stat.timesRested]
+    });
+
+    this.statForm.valueChanges
+      .pipe(debounceTime(700))
+      .pipe(distinctUntilChanged())
+      .subscribe((stats: any) => {
+        this.webSocketService.sendStatsDto(
+           this.createStatsDto(stat.name, stats)
+        )
+      });
+  }
+
+  handleStatsChange(dto : StatsDto) {
+    if (dto.name !== this.currentChar.name) {
+      return;
+    }
+    console.log("consumed stats change in playerbox: "+dto);
+    // let i = this.charStatsList.findIndex( c =>(c.name == dto.name));
+    //this.charCons.find( c: (c.name == charConsum.name)
+    this.currentChar.timesRested = dto.timesRested;
+    this.statForm.setValue({  
+      mightMax: dto.might.maxValue, 
+      mightCur: dto.might.currentValue,
+      speedMax: dto.speed.maxValue,
+      speedCur: dto.speed.currentValue,
+      intellectMax: dto.intellect.maxValue,
+      intellectCur: dto.intellect.currentValue,
+      stressMax: dto.stress.maxValue, 
+      stressCur: dto.stress.currentValue,
+      mentalMax: dto.mental.maxValue,
+      mentalCur: dto.mental.currentValue,
+      timesRested: dto.timesRested
+    }, {emitEvent: false});
+  }
+
+   createStatsDto(name : string, stats: any): StatsDto {
+    var dto  = new StatsDto();
+    dto.name = name;
+    dto.might = new Consumable(stats.mightMax, stats.mightCur);
+    dto.speed = new Consumable(stats.speedMax, stats.speedCur);
+    dto.intellect = new Consumable(stats.intellectMax, stats.intellectCur);
+    dto.stress = new Consumable(stats.stressMax, stats.stressCur);
+    dto.mental = new Consumable(stats.mentalMax, stats.mentalCur);
+    dto.timesRested = stats.timesRested;
+    return dto;
+  }
 
   rest() {
     this.currentChar.timesRested = (this.currentChar.timesRested+1)%5;
     console.log(this.currentChar.timesRested);
-    this.parentStatsForm.get('timesRested')?.setValue(this.currentChar.timesRested);
+    this.statForm.get('timesRested')?.setValue(this.currentChar.timesRested);
     this.pointsToAllocate = Math.floor(Math.random() * 6) + 1;
   }
 
-//this.charCons.find( c: (c.name == charConsum.name)
-
 }
+
 
 
--- rolling-in-space/src/app/player-box/player-box.component.ts
+++ rolling-in-space/src/app/player-box/player-box.component.ts
@@ -1,11 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
+import { FormControl, FormGroup } from '@angular/forms';
 
-import { CharacterConsumables, Consumable } from '../models/entity/character-consumables';
+import { CharacterConsumables } from '../models/entity/character-consumables';
 import { ConsumablesService } from '../services/consumables-service.service';
-import { WebsocketService } from '../services/websocket.service';
-import { StatsDto } from "../models/dto/StatsDto";
 
 @Component({
   selector: 'app-player-box',
@@ -18,95 +15,18 @@ export class PlayerBoxComponent implements OnInit {
   charStatsForm : FormGroup[] = [];
   enableGM : FormControl;
 
-  constructor(
-      private consumableService : ConsumablesService, 
-      private formBuilder: FormBuilder,
-      public webSocketService: WebsocketService) 
-  { 
+  constructor(private consumableService : ConsumablesService) { 
     this.enableGM = new FormControl(false);
   }
 
   ngOnInit(): void {
-    this.webSocketService.connect();
-    this.webSocketService.statChangedEvents.subscribe(dto => {
-      this.handleStatsChange(dto);
-    })
     console.log("init player box");
     this.consumableService.getCharacterConsumables()
       .subscribe((data : any)=>  {
         console.log(data);
         this.charStatsList = data;
-        this.initForms();
     });
   }
 
-  ngOnDestroy(): void {
-    this.webSocketService.disconnect();
-  }
-
-  initForms() {
-    this.charStatsList.forEach( (initialStat, i) => { 
-      this.addStatsForm(initialStat)
-  });
-  }
-
-  //TODO refactor and make it nicer
-  addStatsForm(stat : any) {
-    let numberRegEx =     /^[0-9]\d*$/g ;// /^\d+$/;
-    let validators = [Validators.required, Validators.pattern(numberRegEx)];
-    let form = this.formBuilder.group({
-      mightMax: [stat.might.maxValue, validators],
-      mightCur: [stat.might.currentValue, validators],
-      speedMax: [stat.speed.maxValue, validators],
-      speedCur: [stat.speed.currentValue, validators],
-      intellectMax: [stat.intellect.maxValue, validators],
-      intellectCur: [stat.intellect.currentValue, validators],
-      stressMax: [stat.stress.maxValue, validators],
-      stressCur: [stat.stress.currentValue, validators],
-      mentalMax: [stat.mental.maxValue,  validators],
-      mentalCur: [stat.mental.currentValue,  validators],
-      timesRested: [stat.timesRested]
-    });
-
-    form.valueChanges
-      .pipe(debounceTime(700))
-      .pipe(distinctUntilChanged())
-      .subscribe((stats: any) => {
-        this.webSocketService.sendStatsDto(
-           createStatsDto(stat.name, stats)
-        )
-      });
-    this.charStatsForm.push(form);
-  }
-
-  handleStatsChange(dto : StatsDto) {
-    console.log("consumed stats change in playerbox: "+dto);
-    let i = this.charStatsList.findIndex( c =>(c.name == dto.name));
-    this.charStatsList[i].timesRested = dto.timesRested;
-    this.charStatsForm[i].setValue({  
-      mightMax: dto.might.maxValue, 
-      mightCur: dto.might.currentValue,
-      speedMax: dto.speed.maxValue,
-      speedCur: dto.speed.currentValue,
-      intellectMax: dto.intellect.maxValue,
-      intellectCur: dto.intellect.currentValue,
-      stressMax: dto.stress.maxValue, 
-      stressCur: dto.stress.currentValue,
-      mentalMax: dto.mental.maxValue,
-      mentalCur: dto.mental.currentValue,
-      timesRested: dto.timesRested
-    }, {emitEvent: false});
-  }
-}
-function createStatsDto(name : string, stats: any): StatsDto {
-  var dto  = new StatsDto();
-  dto.name = name;
-  dto.might = new Consumable(stats.mightMax, stats.mightCur);
-  dto.speed = new Consumable(stats.speedMax, stats.speedCur);
-  dto.intellect = new Consumable(stats.intellectMax, stats.intellectCur);
-  dto.stress = new Consumable(stats.stressMax, stats.stressCur);
-  dto.mental = new Consumable(stats.mentalMax, stats.mentalCur);
-  dto.timesRested = stats.timesRested;
-  return dto;
 }
 
